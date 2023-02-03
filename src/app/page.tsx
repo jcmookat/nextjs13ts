90faeedf89ff77e5874ee5875925ac5a15715b43
Jose Cardenas <jscardenas717@rocketmail.com>
2023-02-03 12:11:05 +0900

    base url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import FormPost from './Form'
 
 async function getPosts() {
-	const res = await fetch(`/api/getPosts`)
+	const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
 	if (!res.ok) {
 		console.log(res)
 	}
