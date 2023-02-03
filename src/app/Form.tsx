5af067d113092d8d23974b4884b5f4d3332d1547
Jose Cardenas <jscardenas717@rocketmail.com>
2023-02-03 12:09:01 +0900

    initial commit

---
+++ src/app/Form.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function FormPost() {
+	const [title, setTitle] = useState('')
+	const router = useRouter()
+
+	async function submitPost(e: React.FormEvent) {
+		e.preventDefault()
+		const data = await fetch(`/api/createPost`, {
+			method: 'POST',
+			body: JSON.stringify({ title }),
+		})
+		const res = await data.json()
+		router.refresh()
+		setTitle('')
+		if (!res.ok) console.log(res.message)
+	}
+	return (
+		<form onSubmit={submitPost}>
+			<input
+				onChange={(e) => setTitle(e.target.value)}
+				value={title}
+				type='text'
+			/>
+			<button type='submit' className='btn btn-primary'>
+				Make A new post
+			</button>
+		</form>
+	)
+}
